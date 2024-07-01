0c5f11c2aa708e021b6a13a84ce9462860bc2981
FitriSetiaa <fitrialestari292@gmail.com>
2024-07-01 14:27:29 +0700

    Penambahan Sekolah

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('Halo aku belajar Git di Binar Academy');
 console.log('Halo aku Fitri');
 console.log('Halo aku Setia Lestari');
 console.log('Kelas 12 PPLG A');
+console.log('SMKN 4 PADALARANG')
